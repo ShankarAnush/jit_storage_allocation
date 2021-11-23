e7cf0a74903ddeb8f0236053cf85b01661dd89b6
anushalva3@gmail.com <anushalva3@gmail.com>
2021-11-24 00:58:32 +0100

    redis and cassandra_non_linear testing and training data, restructuring folders

---
+++ storage_allocator/DeepQLearning/redis/fixed_storage_allocator/redis_fixed_storage_allocation.py
@@ -0,0 +1,59 @@
+import pickle
+import csv
+import math
+import pandas as pd
+import numpy as np
+
+
+if __name__ == "__main__":
+    #model = load_model('./models/Kafka_Model.h5')
+    df = pd.read_csv('./raw_data/rabbitmq_testing_data.csv')
+    disk_usage = df['rabbitmq_model'].tolist()
+    current_allocated_disk_space = 0
+    required_disk_space = 0
+    unused_allocated_disk_space = 0
+    total_downtime = 0
+    disk_difference = 0
+    counter = 0
+
+    # Note the allocated disk space, unused allocated disk space and downtime
+    with open(r'rabbitmq_fixed_storage_allocation.csv', 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(['current_allocated_disk_space', 'unused_allocated_disk_space', 'total_downtime', 'original_value'])
+
+    # Note the unused allocated disk space for that instant and do not take the cumulative unused disk space
+    for i in range(0,len(disk_usage)):
+        if i == 0:
+            current_allocated_disk_space = disk_usage[i]
+            required_disk_space = disk_usage[i]
+        else:
+            required_disk_space = disk_usage[i]
+
+        # print("current_allocated_disk_space: {0}, required_disk_space: {1}". format(current_allocated_disk_space, required_disk_space))
+
+        disk_difference = required_disk_space - current_allocated_disk_space
+
+        if disk_difference > 0:
+            current_allocated_disk_space += 5000 # 5 KB in case of rabbitmq
+            unused_allocated_disk_space = disk_difference
+            total_downtime += 1 
+            
+        elif disk_difference < 0:
+            unused_allocated_disk_space = (-1 * disk_difference)
+            # when the disk difference is less than 0, 
+            # application has more allocated disk compared to the required disk space
+            # so no disk space allocation required. hence, no downtime
+            # but there is allocated but unused disk space
+
+        else:
+            pass   # disk space remains to be constant
+
+        with open(r'rabbitmq_fixed_storage_allocation.csv', 'a') as f:
+            writer = csv.writer(f)
+            writer.writerow([current_allocated_disk_space, unused_allocated_disk_space, total_downtime, disk_usage[i] ])
+
+        
+        print("{0}--------------{1}-----------{2}----------{3}".format(current_allocated_disk_space, unused_allocated_disk_space, total_downtime, disk_usage[i]))
+
+
+
---
+++ storage_allocator/DeepQLearning/redis/rl_storage_allocator/redis_main.py
@@ -0,0 +1,83 @@
+from rabbitmq_q_agent import QAgent
+from rabbitmq_storage_allocator import StorageAllocator
+import time
+import os
+import numpy as np
+import gym
+import matplotlib.pyplot as plt
+import random
+import pandas as pd
+import csv
+
+
+def main():
+    window_size = 10
+    episode_count = 100
+    df = "training_data_rabbitmq"
+    batch_size = 32
+    
+    agent = QAgent(window_size)
+    allocator = StorageAllocator(window_size = window_size, dataset = df)
+    
+    cumulative_reward = 0
+    start_time = time.time()
+
+    # Note the rewards and downtime per episode
+    with open(r'rabbitmq_downtime_reward.csv', 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(['rewards_per_episode', 'downtime_per_episode'])
+
+    # Note disk_space every iteration
+    with open(r'rabbitmq_disk_space.csv', 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(['timeslot', 'disk_space'])
+
+    for e in range(episode_count + 1):
+        print("Episode {0}/{1}".format(e, episode_count))
+        agent.reset()
+        state, disk_usage_data = allocator.reset()
+        
+        for t in range(allocator.last_data_index):
+            action, allocated_disk_space = agent.get_action(state, disk_usage_data)
+            
+            next_state, next_disk_space, reward, done = allocator.get_next_state_reward(action, allocated_disk_space)
+            agent.memory.append((state, action, reward, next_state, done))
+            
+            if len(agent.memory) > batch_size:
+                agent.experience_replay(batch_size = batch_size)
+            
+            cumulative_reward += reward
+            state = next_state
+            disk_usage_data = next_disk_space
+            
+            with open(r'rabbitmq_disk_space.csv', 'a') as f:
+                writer = csv.writer(f)
+                writer.writerow([t, allocated_disk_space])
+
+            if done:
+                print("-------------------------------------------------------------------------")
+                print("Total cost incurred due to downtime: {0}".format(agent.get_total_downtime()))
+                print("-------------------------------------------------------------------------")
+                print("-------------------------------------------------------------------------")
+                print("Total reward for this episode: {0}".format(cumulative_reward))
+                print("-------------------------------------------------------------------------")
+                with open(r'rabbitmq_downtime_reward.csv', 'a') as f:
+                    writer = csv.writer(f)
+                    writer.writerow([cumulative_reward, agent.get_total_downtime()])
+
+                with open(r'rabbitmq_disk_space.csv', 'a') as f:
+                    writer = csv.writer(f)
+                    writer.writerow(["-----------------------------------", "--------------------------------------" ])
+            
+    if not os.path.exists("models/rabbitmq_rl_storage_allocator.h5"):
+        os.mkdir("models")
+        agent.model.save("models/rabbitmq_rl_storage_allocator.h5")
+
+    end_time = time.time()
+    train_time = end_time - start_time
+    print("Training time: {} seconds".format(train_time))
+    
+
+if __name__ == "__main__":
+    main()
+
