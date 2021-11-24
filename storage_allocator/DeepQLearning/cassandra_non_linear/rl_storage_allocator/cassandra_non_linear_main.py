f95526a8f8d19ab95fec54904859759dc4af50a9
ShankarAnush <anushalva3@gmail.com>
2021-11-24 02:03:06 +0100

    cassandra non linear rl storage allocation

--- storage_allocator/DeepQLearning/cassandra_non_linear/rl_storage_allocator/cassandra_non_linear_main.py
+++ storage_allocator/DeepQLearning/cassandra_non_linear/rl_storage_allocator/cassandra_non_linear_main.py
@@ -1,5 +1,5 @@
-from rabbitmq_q_agent import QAgent
-from rabbitmq_storage_allocator import StorageAllocator
+from cassandra_non_linear_q_agent import QAgent
+from cassandra_non_linear_storage_allocator import StorageAllocator
 import time
 import os
 import numpy as np
@@ -13,7 +13,7 @@ import csv
 def main():
     window_size = 10
     episode_count = 100
-    df = "training_data_rabbitmq"
+    df = "cassandra_non_linear_testing_data"
     batch_size = 32
     
     agent = QAgent(window_size)
@@ -23,12 +23,12 @@ def main():
     start_time = time.time()
 
     # Note the rewards and downtime per episode
-    with open(r'rabbitmq_downtime_reward.csv', 'w') as f:
+    with open(r'cassandra_non_linear_downtime_reward.csv', 'w') as f:
         writer = csv.writer(f)
         writer.writerow(['rewards_per_episode', 'downtime_per_episode'])
 
     # Note disk_space every iteration
-    with open(r'rabbitmq_disk_space.csv', 'w') as f:
+    with open(r'cassandra_non_linear_disk_space.csv', 'w') as f:
         writer = csv.writer(f)
         writer.writerow(['timeslot', 'disk_space'])
 
@@ -50,7 +50,7 @@ def main():
             state = next_state
             disk_usage_data = next_disk_space
             
-            with open(r'rabbitmq_disk_space.csv', 'a') as f:
+            with open(r'cassandra_non_linear_disk_space.csv', 'a') as f:
                 writer = csv.writer(f)
                 writer.writerow([t, allocated_disk_space])
 
@@ -61,17 +61,17 @@ def main():
                 print("-------------------------------------------------------------------------")
                 print("Total reward for this episode: {0}".format(cumulative_reward))
                 print("-------------------------------------------------------------------------")
-                with open(r'rabbitmq_downtime_reward.csv', 'a') as f:
+                with open(r'cassandra_non_linear_downtime_reward.csv', 'a') as f:
                     writer = csv.writer(f)
                     writer.writerow([cumulative_reward, agent.get_total_downtime()])
 
-                with open(r'rabbitmq_disk_space.csv', 'a') as f:
+                with open(r'cassandra_non_linear_disk_space.csv', 'a') as f:
                     writer = csv.writer(f)
                     writer.writerow(["-----------------------------------", "--------------------------------------" ])
             
-    if not os.path.exists("models/rabbitmq_rl_storage_allocator.h5"):
+    if not os.path.exists("models/cassandra_non_linear_rl_storage_allocator.h5"):
         os.mkdir("models")
-        agent.model.save("models/rabbitmq_rl_storage_allocator.h5")
+        agent.model.save("models/cassandra_non_linear_rl_storage_allocator.h5")
 
     end_time = time.time()
     train_time = end_time - start_time
